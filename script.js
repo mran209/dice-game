447ba23dc2b2508240d405aa44d4302fb247effa
Monica <51390525+mran209@users.noreply.github.com>
2020-12-23 16:19:12 -0600

    Added initial files

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector("button").addEventListener("click", rollDice);
+
+function rollDice() {
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+
+  document.querySelectorAll("img")[0].setAttribute("src", "dice" + randomNumber1 + ".png");
+  document.querySelectorAll("img")[1].setAttribute("src", "dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏆 Player 1 Wins!"
+  } else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🏆"
+  } else {
+    document.querySelector("h1").innerHTML = ("Draw!");
+  }
+}
